caf322f5ac13da4821765b59930fa77192550b3a
kacper <kphalgas@gmail.com>
2019-03-14 16:41:33 +0100

    zadanie2

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
     return 'Hello, World!'
+
+@app.route('/method')
+def method():
+    return request.method
 
 if __name__ == '__main__':
     app.run(debug=True)
