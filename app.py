51bbe13c4d6754c700015324a19d13c280c5d1fd
kacper <kphalgas@gmail.com>
2019-03-14 20:57:46 +0100

    zadanie2

--- app.py
+++ app.py
@@ -13,9 +13,12 @@ def method():
 
 @app.route('/show_data', methods = ['POST'])
 def show_data():
-    x = request.get_json()
-    return jsonify(x)
+    #x = request.get_json()
+    #return jsonify(x)
+    ret = request.get_json()
 
+    resp = Response(response=ret, status=200, mimetype="application/json")
+    return resp
 
 if __name__ == '__main__':
     app.run(debug=True)
