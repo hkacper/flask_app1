274f0c82cad6094c50b77c85cd77cec9186506e8
kacper <kphalgas@gmail.com>
2019-03-14 18:30:15 +0100

    zadanie2

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def method():
 @app.route('/show_data', methods = ['POST'])
 def show_data():
     data = request.get_json()
-    return json.dump(data)
+    print (json.dumps(data))
 
 
 if __name__ == '__main__':
