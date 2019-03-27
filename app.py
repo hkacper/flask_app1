63fc3db9f3777c2c6b91ee7dbac0ae235902bb86
kacper <kphalgas@gmail.com>
2019-03-27 19:07:36 +0100

    123

--- app.py
+++ app.py
@@ -1,46 +1,46 @@
-from flask import Flask, request, jsonify, redirect
-from flask_httpauth import HTTPBasicAuth
-import json
+from flask import Flask, request, jsonify, redirect, session, url_for
 
 app = Flask(__name__)
 app.counter = 0
-auth = HTTPBasicAuth()
-
-users = {
-    "TRAIN":"TuN3L"
-}
-
+app.secret_key = 'VFJBSU46VHVOM0w='
 
 
 @app.route('/', methods = ['GET'])
-def hello():
+def hello2():
     return 'Hello, World!'
+
 
 @app.route('/hello', methods = ['GET'])
 def hello():
     return "Hello World!"
 
+
 @app.route('/method', methods = ['GET', 'POST', 'PUT', 'DELETE'])
 def method():
     return request.method
+
 
 @app.route('/show_data', methods = ['POST'])
 def show_data():    
     return jsonify(request.get_json())
 
+
 @app.route('/pretty_print_name', methods = ["POST"])
 def pretty_print_name():
     return f'Na imię mu {request.get_json()["name"]}, a nazwisko jego {request.get_json()["surename"]}'
 
-@app.route('/login', methods = ['POST'])
-@auth.login_required
-def login():
-    return redirect('https://apka-kurs.herokuapp.com/hello') 
 
 @app.route('/counter')
 def counter():
     app.counter += 1
     return str(app.counter)
+
+@app.route('/login', methods=["POST"])
+def login():
+    if request.session['username'] == 'TRAIN' and request.session['password'] == 'TuN3L':
+        return redirect(url_for('hello2'))
+    return 'invalid username or password'
+
 
 if __name__ == '__main__':
     app.run(debug=True)
