fb47d28a5691377fb68aac8f882e150f1f71795d
kacper <kphalgas@gmail.com>
2019-03-29 19:50:00 +0100

    123

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask, request, jsonify, redirect, session, url_for, Response
 from functools import wraps
-import datetime
+#import datetime
 
 app = Flask(__name__)
-app.permanent_session_lifetime = datetime.timedelta(days=365)
+#app.permanent_session_lifetime = datetime.timedelta(days=365)
 
 def check_auth(username, password):
     return username == 'TRAIN' and password == 'TuN3L'
@@ -48,16 +48,16 @@ def pretty_print_name():
     return f'Na imię mu {request.get_json()["name"]}, a nazwisko jego {request.get_json()["surename"]}'
 
 
-@app.route('/counter')
-def counter():
-    session['visits'] = session.get('visits', 0) + 1
-    return "Visists Count: {}".format(session.get('visits'))
+#@app.route('/counter')
+#def counter():
+#    session['visits'] = session.get('visits', 0) + 1
+#    return "Visists Count: {}".format(session.get('visits'))
 
 @app.route('/login', methods=['POST'])
 @requiered_auth
 def login():
     session['username'] = request.authorization.username
-    return redirect(url_for('hello2'))
+    return redirect(url_for('hello'))
 
 
 if __name__ == '__main__':
