4f494b5c6b0b46d7d218280d4e668191799af4c8
kacper <kphalgas@gmail.com>
2019-04-03 20:29:08 +0200

    123

--- app.py
+++ app.py
@@ -144,12 +144,12 @@ def tracks_list():
                     data = cursor.execute("""SELECT tracks.name FROM tracks
                                     JOIN albums ON tracks.albumid = albums.albumid
                                     JOIN artists ON albums.artistid = artists.artistid 
-                                    WHERE artists.name = ? ORDER BY tracks.name LIMIT ? OFFSET ? COLLATE NOCASE""", (artist,per_page, per_page)).fetchall()
+                                    WHERE artists.name = ? ORDER BY tracks.name LIMIT ? OFFSET ? COLLATE NOCASE""", (artist,per_page, per_page+1)).fetchall()
                 else:
                     data = cursor.execute("""SELECT tracks.name FROM tracks
                                     JOIN albums ON tracks.albumid = albums.albumid
                                     JOIN artists ON albums.artistid = artists.artistid 
-                                    WHERE artists.name = ? ORDER BY tracks.name LIMIT ? OFFSET ? COLLATE NOCASE""", (artist,per_page, ((page-1)*per_page))).fetchall()
+                                    WHERE artists.name = ? ORDER BY tracks.name LIMIT ? OFFSET ? COLLATE NOCASE""", (artist,per_page, ((page-1)*per_page+1))).fetchall()
             elif request.args.get('per_page') and  request.args.get('artist'):
                 per_page = request.args.get('per_page')
                 artist = request.args.get('artist')
@@ -161,7 +161,7 @@ def tracks_list():
                 per_page = request.args.get('per_page')
                 page = request.args.get('page')
                 data = cursor.execute("""SELECT name FROM tracks
-                                        ORDER BY name LIMIT ? OFFSET ? COLLATE NOCASE""", (per_page, ((page-1)*per_page))).fetchall()
+                                        ORDER BY name LIMIT ? OFFSET ? COLLATE NOCASE""", (per_page, ((page-1)*per_page+1))).fetchall()
             elif request.args.get('artist'):
                 artist = request.args.get('artist')
                 data = cursor.execute("""SELECT tracks.name FROM tracks
