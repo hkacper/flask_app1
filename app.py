409557cdad22f4ac22f03ec01446d44c72cf2923
kacper <kphalgas@gmail.com>
2019-04-03 18:44:45 +0200

    123

--- app.py
+++ app.py
@@ -136,16 +136,22 @@ def tracks_list():
     db = get_db()
     cursor = db.cursor()
     if request.method == 'GET':
-            # if request.args.get('per_page'):
-            #     per_page = request.args.get('per_page')
-            if request.args.get('artist'):
+            if request.args.get('per_page') and request.args.get('page') and request.args.get('artist'):
+                per_page = request.args.get('per_page')
+                page = request.args.get('page')
                 artist = request.args.get('artist')
                 data = cursor.execute("""SELECT tracks.name FROM tracks
                                     JOIN albums ON tracks.albumid = albums.albumid
                                     JOIN artists ON albums.artistid = artists.artistid 
-                                    WHERE artists.name = ? ORDER BY tracks.name  COLLATE NOCASE""", (artist,)).fetchall()
-        # else:
-        #     data = cursor.execute('SELECT Name FROM tracks ORDER BY Name COLLATE NOCASE').fetchall()
+                                    WHERE artists.name = ? ORDER BY tracks.name LIMIT ? OFFSET ? COLLATE NOCASE""", (artist,per_page, page*per_page)).fetchall()
+            elif request.args.get('artist'):
+                artist = request.args.get('artist')
+                data = cursor.execute("""SELECT tracks.name FROM tracks
+                                    JOIN albums ON tracks.albumid = albums.albumid
+                                    JOIN artists ON albums.artistid = artists.artistid 
+                                    WHERE artists.name = ? ORDER BY tracks.name COLLATE NOCASE""", (artist,)).fetchall()
+            else:
+                data = cursor.execute('SELECT Name FROM tracks ORDER BY Name COLLATE NOCASE').fetchall()
     cursor.close()
     tracks = [track[0] for track in data]
     return jsonify(tracks)
